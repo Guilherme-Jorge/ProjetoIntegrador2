fe68392e7ce8ab26809b524c76a51c501c45babd
BuildTools <unconfigured@null.spigotmc.org>
2022-10-23 19:10:07 -0300

    Implementei a geração de bilhete funcional

--- scripts/gerar.js
+++ scripts/gerar.js
@@ -3,12 +3,3 @@ fetch("http://localhost:3333/api/bilhete")
   .then((res) => {
     document.getElementById("cod").innerHTML = res.id;
   });
-
-// async function banana() {
-//   const chamada = await fetch("http://localhost:3333/api/bilhete").then(
-//     (data) => data.json()
-//   );
-//   return (document.getElementById("cod").innerHTML = chamada.id);
-// }
-
-// banana();
